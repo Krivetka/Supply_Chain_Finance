94e56fa7902075d3a17580ec75ab74318c994eb7
ikuliashov <dfyreybr@gmail.com>
2026-07-07 12:34:53 +0200

    feat: add invoicing ui components with signal inputs and outputs

--- libs/invoicing/ui/src/index.ts
+++ libs/invoicing/ui/src/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { InvoiceRowComponent } from './lib/invoice-row.component';
+export { InvoiceFiltersComponent } from './lib/invoice-filters.component';
+export { FinanceButtonComponent } from './lib/finance-button.component';
