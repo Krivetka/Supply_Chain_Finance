a246c679283b5b693912133b5b7ebb8137870ada
ikuliashov <dfyreybr@gmail.com>
2026-07-07 12:11:19 +0200

    feat: add value objects and exhaustive helper

--- libs/shared/kernel/src/index.ts
+++ libs/shared/kernel/src/index.ts
@@ -1,1 +1,13 @@
-export {};
+export type { Currency } from './lib/currency';
+export { currency, isCurrency } from './lib/currency';
+
+export type { Money } from './lib/money';
+export { money, addMoney, subtractMoney, formatMoney } from './lib/money';
+
+export type { SupplierId, InvoiceId, UserId } from './lib/ids';
+export { supplierId, invoiceId, userId } from './lib/ids';
+
+export type { DateTime } from './lib/date-time';
+export { dateTime, isPast } from './lib/date-time';
+
+export { assertNever } from './lib/exhaustive';
