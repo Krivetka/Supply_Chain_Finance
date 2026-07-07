f8dfbefbdaba3e4db63ce689aa2f8bd5b8e5d23d
ikuliashov <dfyreybr@gmail.com>
2026-07-07 12:19:56 +0200

    feat: add AuthStore with permissions and fixture users

--- libs/auth/data-access/src/index.ts
+++ libs/auth/data-access/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+export type { Permission } from './lib/permissions';
+export { ALL_PERMISSIONS } from './lib/permissions';
+
+export type { User } from './lib/user';
+
+export { AuthStore } from './lib/auth.store';
+
+export { ACME_SUPPLIER, BETA_SUPPLIER } from './lib/auth.fixtures';
