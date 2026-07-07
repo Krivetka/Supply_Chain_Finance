68ee761c13a4d51b3d11a67e56921cf3f289745a
ikuliashov <dfyreybr@gmail.com>
2026-07-07 12:17:47 +0200

    feat: invoice union, eligibility predicate, result union

--- libs/invoicing/domain/src/index.ts
+++ libs/invoicing/domain/src/index.ts
@@ -1,1 +1,18 @@
-export {};
+export type { FinancingOffer, FinancingErrorCode, FinancingRequested, FinancingError, RequestFinancingResult } from './lib/financing.types';
+
+export type {
+  DraftInvoice,
+  ApprovedInvoice,
+  FinancingRequestedInvoice,
+  FinancedInvoice,
+  PaidInvoice,
+  RejectedInvoice,
+  Invoice,
+} from './lib/invoice.types';
+export { InvoiceStatus } from './lib/invoice.types';
+
+export type { EligibleInvoice, EligibilityActor } from './lib/eligibility';
+export { canRequestFinancing, FINANCING_PERMISSION } from './lib/eligibility';
+
+export type { InvoiceFilter } from './lib/filters';
+export { EMPTY_FILTER, matchesFilter } from './lib/filters';
