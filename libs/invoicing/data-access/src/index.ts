0b6ddc3a57635c94f764f1f4f0a158ab9c98d6d7
ikuliashov <dfyreybr@gmail.com>
2026-07-07 12:30:20 +0200

    feat: add invoicing data-access layer with SignalStore and mock service

--- libs/invoicing/data-access/src/index.ts
+++ libs/invoicing/data-access/src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export type { RawInvoice, RawFinancingOffer } from './lib/raw-invoice';
+export { mapInvoice } from './lib/invoices.mapper';
+export { InvoicesService } from './lib/invoices.service';
+export { InvoicesStore } from './lib/invoices.store';
+export { INVOICE_FIXTURES } from './lib/fixtures';
